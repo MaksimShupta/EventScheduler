b432327334fed13ccfbb9cb56284281617490ff5
“Aniko” <aniko.juhasz@yahoo.com>
2025-02-10 15:56:01 +0100

    Sync with dev & added font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#33BFCC", // Turquoise
+                secondary: "#A47864", // Brown
+                accent: "#E3D5CA", // Light beige
+                bgDark: "#1E1E1E", // Main dark background
+                bgLight: "#282828", // Slightly lighter dark background
+                light: "#F5F5F5", // Almost white for contrast
+            },
+            fontFamily: {
+                sans: ["Inter", "sans-serif"], // Google Font "Inter"
+            },
+        },
+    },
+    plugins: [daisyui],
 };
