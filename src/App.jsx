0ba7612d9f9864735ae9bf6ef02b8f18f904d450
Maksim Shupta <maxshupta@yahoo.com>
2025-02-14 15:30:14 +0100

    errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
